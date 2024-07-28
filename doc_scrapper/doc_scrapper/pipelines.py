56a7b15a89e07c9cc244e62b31a817d96ba35855
VencaH <venca.hyncica@gmail.com>
2024-07-28 22:10:25 +0000

    added pipeline items to check if there is text and to save it to folders

--- doc_scrapper/doc_scrapper/pipelines.py
+++ doc_scrapper/doc_scrapper/pipelines.py
@@ -6,8 +6,52 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+import os
 
 
-class TutorialPipeline:
+class TextValidationPipeline:
+    def __init__(self):
+        print("Pipeline initialized")
+
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter.get("text") and adapter.get("text") != "":
+            return item
+        else:
+            print("got nothing back")
+            raise DropItem(f"Missing or empty text field, no need for further processing")
+
+class SaveToFolderPipeline:
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        
+        if not (adapter.get("route")) or adapter.get("route") == "":
+            raise DropItem(f"Missing or empty route field, can't save the file")
+        if not (adapter.get("page")) or adapter.get("page") == "":
+            raise DropItem(f"Missing or empty page field, can't save the file")
+        
+        folders = item.get("route")
+        path = "/".join(folders)
+        filename = item.get("page")
+        text = item.get("text")
+        print(path)
+        self.ensure_path_exists(folders)
+        self.save_to_file(path, filename, text)
+
         return item
+        
+    def ensure_path_exists(self, folders):
+        print("folders:")
+        print(folders)
+        for i in range(len(folders)):
+            path = "/".join(folders[:i+1])
+            if not os.path.exists(path):
+                os.makedirs(path) 
+
+    def save_to_file(self, path, filename, text):
+        full_path = f"{path}/{filename}.txt"
+        with open(full_path, "w") as outfile:
+            outfile.write(text)
+        
--- doc_scrapper/doc_scrapper/settings.py
+++ doc_scrapper/doc_scrapper/settings.py
@@ -62,9 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "tutorial.pipelines.TutorialPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "doc_scrapper.pipelines.TextValidationPipeline": 300,
+   "doc_scrapper.pipelines.SaveToFolderPipeline": 600,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
